721e999d6a56c485e6b6235b670fba9bbc3184b3
M Faisal Hayat <m.faisalhayat3@gmail.com>
2022-10-10 21:12:02 +0500

    basics are done

--- basics/linear_regression.py
+++ basics/linear_regression.py
@@ -1,5 +1,5 @@
 # import libraries
-import os 
+import os
 import sys
 import tqdm
 import torch
@@ -12,13 +12,14 @@ print(f'numpy version is  :{np.__version__}')
 print(f'pandas version is  :{pd.__version__}')
 print(f'torch version is : {torch.__version__}')
 
-def main():
-    x_train = np.array([[3.3], [4.4], [5.5], [6.71], [6.93], [4.168], 
-                    [9.779], [6.182], [7.59], [2.167], [7.042], 
-                    [10.791], [5.313], [7.997], [3.1]], dtype=np.float32)
 
-    y_train = np.array([[1.7], [2.76], [2.09], [3.19], [1.694], [1.573], 
-                        [3.366], [2.596], [2.53], [1.221], [2.827], 
+def main():
+    x_train = np.array([[3.3], [4.4], [5.5], [6.71], [6.93], [4.168],
+                        [9.779], [6.182], [7.59], [2.167], [7.042],
+                        [10.791], [5.313], [7.997], [3.1]], dtype=np.float32)
+
+    y_train = np.array([[1.7], [2.76], [2.09], [3.19], [1.694], [1.573],
+                        [3.366], [2.596], [2.53], [1.221], [2.827],
                         [3.465], [1.65], [2.904], [1.3]], dtype=np.float32)
 
     # Define the model 
@@ -26,7 +27,7 @@ def main():
     input_size = 1
     output_size = 1
     learning_rate = 0.01
-    model = nn.Linear(in_features=input_size, 
+    model = nn.Linear(in_features=input_size,
                       out_features=output_size)
     loss = nn.MSELoss()
     optimizer = torch.optim.Adam(model.parameters(),
@@ -42,10 +43,10 @@ def main():
         optimizer.zero_grad()
         l.backward()
         optimizer.step()
-        if epoch % 5==0:
+        if epoch % 5 == 0:
             print(f'loss is  :{l.item()}')
-    
-    # Let's deiplay the model parameters 
+
+    # Let's display the model parameters
     for name, param in model.named_parameters():
         if param.requires_grad:
             print(name, param.data)
